fa981a84275407fb4b1ca14d86c227064a174392
Luiz Paro <lg_paro@hotmail.com>
2016-06-06 14:09:46 -0300

    Configuring Spring MVC in the project.

---
+++ src/main/java/br/com/devmedia/blog/config/SpringWebXmlConfig.java
@@ -0,0 +1,33 @@
+package br.com.devmedia.blog.config;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class SpringWebXmlConfig implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContex) throws ServletException {
+        AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
+        webContext.register(SpringMvcConfig.class);
+        webContext.setServletContext(servletContex);
+        
+        DispatcherServlet dispatcher = new DispatcherServlet(webContext);
+        dispatcher.setThrowExceptionIfNoHandlerFound(true);
+        
+        ServletRegistration.Dynamic dynamicRegistration = servletContex.addServlet("dispatcher", dispatcher);
+        dynamicRegistration.setLoadOnStartup(1);
+        dynamicRegistration.addMapping("/");
+
+        FilterRegistration.Dynamic encodingFilter = servletContex.addFilter("encodingFilter", new CharacterEncodingFilter());
+        encodingFilter.setInitParameter("encoding", "UTF-8");
+        encodingFilter.setInitParameter("forceEncoding", "true");
+        encodingFilter.addMappingForUrlPatterns(null, true, "/*");
+    }
+}
