2a49757fade5f4fb33d0ce335764ee47ff4cdf7e
Luiz Paro <lg_paro@hotmail.com>
2016-06-24 12:11:47 -0300

    Removing unnecessary imports.

--- src/main/java/br/com/devmedia/blog/web/controller/UsuarioController.java
+++ src/main/java/br/com/devmedia/blog/web/controller/UsuarioController.java
@@ -1,7 +1,6 @@
 package br.com.devmedia.blog.web.controller;
 
 import java.io.Serializable;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
